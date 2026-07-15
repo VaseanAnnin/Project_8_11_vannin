0491e034a3b43c05cebce8ee1b0d9e8449907c21
Vasean Annin <vaseanannin@gmail.com>
2026-07-15 19:15:25 -0400

    added functionality in test_song to test if the song is being created the right way and if the information is the same as what was put into it

--- tests/test_song.py
+++ tests/test_song.py
@@ -3,6 +3,29 @@ from song import Song
 
 class TestSong(unittest.TestCase):
 
+
+
+    def test_add_song(self):
+        
+        song = Song(
+            "Freudian",
+            "Daniel Caesar",
+            "Guitar",
+            "RNB",
+            10
+        )
+
+        self.assertEqual(song.title, 'Freudian')
+
+        self.assertEqual(song.artist, 'Daniel Caesar')
+
+        self.assertEqual(song.instrument, 'Guitar')
+
+        self.assertEqual(song.genre, 'RNB')
+
+        self.assertEqual(song.progress, 10)
+
+        
     def test_update_progress(self):
 
         song = Song(
@@ -16,3 +39,4 @@ class TestSong(unittest.TestCase):
         song.update_progress(50)
 
         self.assertEqual(song.progress, 50)
+    
